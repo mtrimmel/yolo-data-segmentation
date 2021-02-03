9278fa3c1bc6e93ce5189bb74d1d1482b6e10a85
HansiIMW <hans.kueffner-mccauley@tuwien.ac.at>
2021-02-03 19:01:55 +0100

    Hans: merge branch master

--- write.py
+++ write.py
@@ -17,7 +17,7 @@ import cv2
 import math
 
 
-def write_obj(path, label_path, variables, xC, yC, img, i):
+def write_obj(path, variables, xC, yC, img, i):
     """
     Saves the overlay image and the associated object label
 
@@ -49,7 +49,7 @@ def write_obj(path, label_path, variables, xC, yC, img, i):
     
     text = '%d %f %f %f %f\n' % (name, xC1, yC1, rel_width, rel_height)
 
-    label = os.path.join(label_path, '%d_%.2f_%d.txt' % (name,radius, i))
+    label = os.path.join(path, '%d_%.2f_%d.txt' % (name,radius, i))
 
     pic_path = os.path.join(path, '%d_%.2f_%d.jpg' % (name,radius, i))
 
